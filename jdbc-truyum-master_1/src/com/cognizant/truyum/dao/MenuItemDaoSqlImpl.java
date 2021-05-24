2422b53ebb249e9dd1289fd466dbad3e48c65f22
sanmaddy445 <62550368+sanmaddy445@users.noreply.github.com>
2021-05-24 12:05:09 +0530

    Add files via upload

---
+++ jdbc-truyum-master_1/src/com/cognizant/truyum/dao/MenuItemDaoSqlImpl.java
@@ -0,0 +1,17 @@
+package com.cognizant.truyum.dao;
+import java.sql.Date;
+
+import java.sql.*;
+import java.util.*;
+
+import com.cognizant.truyum.model.MenuItem;
+
+public  class MenuItemDaoSqlImpl  {
+	
+	public ArrayList<MenuItem> getMenuItemListAdmin()
+	{
+	    ArrayList<MenuItem> menuItemList = new ArrayList<MenuItem>();
+	    
+
+	}
+}
