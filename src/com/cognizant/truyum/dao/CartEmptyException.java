71239f1c8cdb94a8b6dd8ee5b6c6c16a3b3d0b28
sanmaddy445 <62550368+sanmaddy445@users.noreply.github.com>
2021-05-24 12:09:19 +0530

    Delete src directory

--- src/com/cognizant/truyum/dao/CartEmptyException.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.truyum.dao;
-
-@SuppressWarnings("serial")
-public class CartEmptyException extends Throwable{
-
-	CartEmptyException(String message){
-		super(message);
-	}
-}
